bd737fecdb6781b20cfbe5510cc604a4defef4d6
EwboKing <hjm2402108258@sina.com>
2021-10-23 15:42:02 +0800

    完成登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button @click="logout" type="info">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    logout () {
+      // 清除token
+      window.sessionStorage.clear()
+      // 重定向到登录页面
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avater_box">
+        <img src="../assets/logo.png">
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" placeholder="输入你的用户名" prefix-icon="iconfont icon-user-fill-copy">
+          </el-input>
+        </el-form-item>
+
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" placeholder="输入你的密码" type="password"
+            prefix-icon="iconfont icon-lock2-copy"></el-input>
+        </el-form-item>
+
+        <!-- 按钮区 -->
+        <el-form-item class="btns">
+          <el-button @click="login" type="primary" round>登录</el-button>
+          <el-button @click="resetLoginForm" type="info" round>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单的验证规则
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [{
+          required: true,
+          message: '请输入登录用户名',
+          trigger: 'blur'
+        },
+        {
+          min: 5,
+          max: 12,
+          message: '长度在 5 到 12 个字符',
+          trigger: 'blur'
+        }
+        ],
+        // 验证密码是否合法
+        password: [{
+          required: true,
+          message: '请输入登录密码',
+          trigger: 'blur'
+        },
+        {
+          min: 6,
+          max: 15,
+          message: '长度在 6 到 15 个字符',
+          trigger: 'blur'
+        }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮重置登录表单
+    resetLoginForm () {
+      // console.log(this)
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      // axios.post('http://127.0.0.1:8888/api/private/v1/login', this.loginForm).then(
+      //   response => {
+      //     // console.log(response.data)
+      //     if (response.data.meta.status !== 200) return this.$message.error('登录失败')
+      //     this.$message.success('登录成功')
+      //     // 1.将登录成功之后的 tolen，保存到客户端的 sessionStorage中
+      //     //    1.1项目中出了登录之外的其他AP接口，必须在登录之后才能访问
+      //     //    1.2 token只应在当前网站打开期间生效，所以将 token保存在 sessionstorage中/
+      //     // 2.通过编程式导航跳转到后台主页，路由地址是/home
+      //   },
+      //   error => {
+      //     console.log(error.message)
+      //   }
+      // )
+      this.$refs.loginFormRef.validate(async valid => {
+        // console.log(this)
+        if (!valid) return
+        const { data: res } = await this.$http.post('http://127.0.0.1:8888/api/private/v1/login', this.loginForm)
+        // console.log(res.meta)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        // 1.将登录成功之后的 tolen，保存到客户端的 sessionStorage中
+        //    1.1项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        //    1.2 token只应在当前网站打开期间生效，所以将 token保存在 sessionstorage中/
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2.通过编程式导航跳转到后台主页，路由地址是/home
+        this.$router.push('/home')
+      })
+    }
+  }
+
+}
+</script>
+
+<style lang="less" scoped>
+  .login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    .avater_box {
+      height: 130px;
+      width: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+</style>
